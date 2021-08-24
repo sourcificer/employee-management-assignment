031f1aeebd80bd9b01cc499e29b383af88697ba6
sourcificer <harshys.here@gmail.com>
2021-08-24 08:52:49 +0530

    added a component to add employee - not compelte yet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,28 @@
-import './App.css';
-import { Text, Tabs, Tab, TabList, TabPanel, TabPanels } from '@chakra-ui/react';
-import DisplayEmployees from './components/DisplayEmployees';
+import "./App.css";
+import { Text, Tabs, Tab, TabList, TabPanel, TabPanels } from "@chakra-ui/react";
+import DisplayEmployees from "./components/DisplayEmployees";
+import AddEmployee from "./components/AddEmployee";
 
 function App() {
   return (
     <div className="App">
-      <Text
-      bgGradient="linear(to-l, #7928CA,#FF0080)"
-      bgClip="text"
-      fontSize="6xl"
-      fontWeight="extrabold"
-      >
+      <Text bgGradient="linear(to-l, #7928CA,#FF0080)" bgClip="text" fontSize="6xl" fontWeight="extrabold">
         Employee Management
       </Text>
       <Tabs variant="soft-rounded" colorScheme="blue">
-      <TabList>
-        <Tab>Employee List</Tab>
-        <Tab>Add Employee</Tab>
-      </TabList>
-      <TabPanels>
-        <TabPanel>
-          <DisplayEmployees />
-        </TabPanel>
-        <TabPanel>
-          <p>two!</p>
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
+        <TabList>
+          <Tab>Employee List</Tab>
+          <Tab>Add Employee</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <DisplayEmployees />
+          </TabPanel>
+          <TabPanel>
+            <AddEmployee />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </div>
   );
 }
--- client/src/components/AddEmployee.jsx
+++ client/src/components/AddEmployee.jsx
@@ -1,23 +1,49 @@
-import React, { useState } from 'react';
-import { createData } from '../employee.service';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Input, Button } from "@chakra-ui/react";
 
 const AddEmployee = () => {
-  const initialEmployeeData = {
-    name: "",
-    role: "",
-    manager: "",
-    office: "",
-    joining_date: "",
-    mobile: "",
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = (data) => {
+    const requestData = {
+      employee_data: data,
+    };
+    console.log(JSON.stringify(requestData));
+    saveEmployee(requestData);
   };
-  const [employee, setEmployee] = useState(initialEmployeeData);
-  
-  const saveEmployee = () => {
-    createData(employee).then(res => {
-      console.log(res.data);
-    }).catch(err => {
-      console.log(err);
+
+  const saveEmployee = (requestBody) => {
+    fetch(`${process.env.REACT_APP_API_URL}/employees/`, {
+      method: "POST",
+      body: JSON.stringify(requestBody),
     })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((jsonResponse) => {
+        console.log(jsonResponse);
+        reset();
+      })
+      // eslint-disable-next-line
+      .catch((err) => console.log(err));
   };
-  
-}
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Input variant="outline" type="text" {...register("name", { required: true, maxLength: 100 })} />
+      <Input variant="outline" type="text" {...register("role", { required: true })} />
+      <Input variant="outline" type="text" {...register("manager", { required: true })} />
+      <Input variant="outline" type="text" {...register("office", { required: true })} />
+      <Input variant="outline" type="tel" {...register("mobile", { maxLength: 12 })} />
+      <Input variant="outline" type="date" {...register("joining_date", {})} />
+
+      <Button colorScheme="blue" type="submit">
+        Submit
+      </Button>
+    </form>
+  );
+};
+
+export default AddEmployee;
